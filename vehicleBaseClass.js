613a2ef4927a74bb374ec37cc067e85ed26d4cbe
TimothyLewis <timlew2003@gmail.com>
2024-09-06 11:49:07 -0700

    Added Car class with required properties and methods

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -84,6 +84,46 @@ class Vehicle {
         }
     }
 }
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);  // Call the parent class constructor
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger(num) {
+        if (this.passengers + num <= this.maximumPassengers) {
+            this.passengers += num;
+            console.log(`${num} passengers loaded. Current passengers: ${this.passengers}`);
+        } else {
+            console.log("Not enough room to load all passengers.");
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            this.started = true;
+            console.log("Car started!");
+        } else {
+            this.started = false;
+            console.log("Not enough fuel to start the car.");
+        }
+    }
+
+    scheduleServiceCheck(mileage) {
+        if (mileage > 30000) {
+            this.scheduleService = true;
+            console.log("Service required!");
+        } else {
+            console.log("No service needed.");
+        }
+    }
+}
+
 
 //This exports things you want to use from this "module", more info in readme
 module.exports = {
